0497050c20c9f020a9bfcb8ddeb575a96e46e405
Ashley MacWhirter <macwhirtera@gmail.com>
2017-06-25 17:13:01 -0400

    hamburger menu fixed

--- app/public/javascript/app.js
+++ app/public/javascript/app.js
@@ -1,19 +1,26 @@
 $(document).ready(function() {
 
-$( ".cross" ).hide();
-$( ".menu" ).hide();
-$( ".hamburger" ).click(function() {
-$( ".menu" ).slideToggle( "slow", function() {
-$( ".hamburger" ).hide();
-$( ".cross" ).show();
-});
-});
+    $(".cross").hide();
+    $(".menu").hide();
+    $(".hamburger").click(function() {
+        $(".menu").slideToggle("slow", function() {
+            $(".hamburger").hide();
+            $(".cross").show();
+        });
+    });
 
-$( ".cross" ).click(function() {
-$( ".menu" ).slideToggle( "slow", function() {
-$( ".cross" ).hide();
-$( ".hamburger" ).show();
-});
-});
+    $(".cross").click(function() {
+        $(".menu").slideToggle("slow", function() {
+            $(".cross").hide();
+            $(".hamburger").show();
+        });
+    });
+
+    $(".menu").click(function() {
+        $(".menu").slideToggle("slow", function(){
+          $(".cross").hide();
+          $(".hamburger").show();
+        });
+    });
 
 });
