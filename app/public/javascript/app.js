c6e9f7d4af44d357ba73d5aa30434cbacde96048
Ashley MacWhirter <macwhirtera@gmail.com>
2017-07-12 12:47:53 -0400

    thumbnail hover

--- app/public/javascript/app.js
+++ app/public/javascript/app.js
@@ -51,6 +51,42 @@ $(document).ready(function() {
         $("#previewGit").attr("href", git);
     });
 
+//========== MODAL HOVER =============
+
+// loop through each image that is a link
+  $( 'data img' ).each(function(){
+
+     // wrap the image link in a div
+     $( this ).parent().wrap( '<div class="img-link-wrapper" style="display: inline-block; text-align: center;"></div>' );
+
+     // set the height of the div to the height of the image
+     var imgHeight = $( this ).height();
+     var imgWidth = $( this ).width();
+     $( this ).parents( 'div.img-link-wrapper' )
+       .css({ 'height' : imgHeight,
+              'width' : imgWidth
+            });
+
+     // set the rest of the initial css
+    $( this ).css({ 'height' : '94%',
+                    'width' : 'auto',
+                    'margin' : '3% 0'
+                  });
+
+  });
+
+  // set up a .hovered class
+  $( 'body' ).append( '<style>img.img-link-hovered{ margin-top: 0 !important; margin-bottom: 0 !important; height: 100% !important; }</style>' );
+
+  // add/remove the class on hover
+  $( 'data img' ).hover(
+    function() {
+    $( this ).addClass( 'img-link-hovered' );
+    }, function() {
+      $( this ).removeClass( 'img-link-hovered' );
+    }
+  );
+
 //========== TYPEWRITER FOR HOME COVER PAGE =============
 
 function setupTypewriter(t) {
